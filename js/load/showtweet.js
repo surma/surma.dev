b64882ce37f6d103b0dc8592e3cd52d313d5911b
Alexander Surma <alexander.surma@gmail.com>
2011-08-23 20:09:33 +0200

    Added twitter follow button

--- js/load/showtweet.js
+++ js/load/showtweet.js
@@ -1,1 +1,4 @@
-MyTwitter.getTweet('surmair', '.mytwitter');
+(function($) {
+	var username = $.grep($(".twitter-follow-button").attr('src').split('&'), function(elem, idx){return elem.indexOf('screen_name') == 0;})[0].split('=')[1];
+	MyTwitter.getTweet(username, '.mytwitter');
+})(jQuery);
