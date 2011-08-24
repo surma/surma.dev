32727d72b51d0754982262cdb686cad71df666a8
Alexander Surma <alexander.surma@gmail.com>
2011-08-24 15:33:24 +0200

    Added 3 social plugins

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
 (function(Loader) {
 	// Less
-	Loader
-	.script('js/load/less.js');
+	Loader.script('js/load/less.js');
 
 	// MyTwitter
 	Loader
+	.setOptions('AlwaysPreserveOrder')
 	.script('js/load/jquery.js')
-	.script('http://platform.twitter.com/widgets.js')
-	.wait()
 	.script('js/load/mytwitter.js')
-	.wait()
 	.script('js/load/showtweet.js');
 
-	// Comments
-	Loader
-	.script('http://connect.facebook.net/en_US/all.js#xfbml=1');
+	// Social Plugins
+	Loader.script('http://platform.twitter.com/widgets.js')
+	Loader.script('https://apis.google.com/js/plusone.js')
+	Loader.script('http://connect.facebook.net/en_US/all.js#xfbml=1');
 
 	// Media Query polyfill
-	Modernizr.mq('(min-width)') ||
-	Loader
-	.script('js/load/respond.js');
+	Modernizr.mq('(min-width)')
+	|| Loader.script('js/load/respond.js');
 })($LAB);
