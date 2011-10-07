bcee5b3da028c12bc3310b0d016ed896b689c4ad
Alexander Surma <alexander.surma@gmail.com>
2011-10-07 13:27:57 +0200

    v1.0 design

--- js/load/linkextractor.js
+++ js/load/linkextractor.js
@@ -4,13 +4,23 @@
 		return s.replace(/^(https?|ftps?|mailto):(\/\/)?(www\.)?/, '');
 	}
 
+	var $target = $('#relatedlinks');
+	if($target.length <= 0) {
+		return;
+	}
 	var $c = $('<dl>').attr('id', 'relatedlinkslist');
 	$('#main a').each(function() {
+		$(this).attr('target', "_blank");
 		var $dt = $('<dt>');
 		var $dd = $('<dd>');
 		var href = $(this).attr('href');
 		var shortlink = shorten(href);
 		var desc = $(this).attr('title');
+
+		if(shortlink.length > 30) {
+			var len = shortlink.length;
+			shortlink = shortlink.substr(0,20)+"..."+shortlink.substr(len-8, len);
+		}
 
 		$dd.text(desc);
 		var $link = $("<a>")
--- js/main.js
+++ js/main.js
@@ -15,8 +15,4 @@
 	Loader.script('http://platform.twitter.com/widgets.js')
 	Loader.script('https://apis.google.com/js/plusone.js')
 	Loader.script('http://connect.facebook.net/en_US/all.js#xfbml=1');
-
-	// Media Query polyfill
-	Modernizr.mq('(min-width)')
-	|| Loader.script('/js/load/respond.js');
 })($LAB);
