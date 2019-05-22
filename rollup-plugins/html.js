e965cfd8e43acedcaefa930508089017df8c3ebe
Surma <surma@surma.link>
2019-05-22 22:02:23 +0100

    Parse html file

--- rollup-plugins/html.js
+++ rollup-plugins/html.js
@@ -1,6 +1,8 @@
 import * as fs from "fs";
-import { join, basename } from "path";
+import { join } from "path";
 import { createFilter } from "rollup-pluginutils";
+import { parse as parseHTML, serialize as serializeHTML } from "parse5";
+import { stringLiteral } from "babel-types";
 
 const defaultOpts = {
   include: "**/*.html",
@@ -8,55 +10,115 @@ const defaultOpts = {
   minify: true
 };
 
+function findAllNodes(node, predicate) {
+  const result = [];
+  if (predicate(node)) {
+    result.push(node);
+  }
+  if (node.childNodes) {
+    node.childNodes.forEach(node => {
+      result.push(...findAllNodes(node, predicate));
+    });
+  }
+  return result;
+}
+
+function isExternalScript(node) {
+  return node.nodeName.toLowerCase() === "script" &&
+    node.attrs.some(attr => attr.name === "src");
+}
+
+function isExternalStylesheet(node) {
+  return node.nodeName.toLowerCase() === "link" &&
+    node.attrs.some(attr => attr.name === "rel" && attr.value === "stylesheet");
+}
+
+async function findScriptTags(doc, importee) {
+  return Promise.all(findAllNodes(doc, isExternalScript).map(async node => {
+    let attr = node.attrs.find(attr => attr.name === "src");
+    let path = attr.value;
+    if (path.startsWith("/")) {
+      path = join(
+        // FIXME lol
+        "/Users/surma/src/github.com/surma/surma.github.io/_site/",
+        path
+      );
+    }
+    const { id: importId } = await this.resolve(path, importee);
+    const chunkId = this.emitChunk(importId);
+    // attr.value = `${placeholderPrefix}${chunkId}${placeholderSuffix}`;
+    attr.value = chunkId;
+    return attr;
+  }));
+}
+
+async function findStylesheets(doc, importee) {
+  return await Promise.all(findAllNodes(doc, isExternalStylesheet).map(async node => {
+    let attr = node.attrs.find(attr => attr.name === "href");
+    let path = attr.value;
+    if (path.startsWith("/")) {
+      path = join(
+        // FIXME lol
+        "/Users/surma/src/github.com/surma/surma.github.io/_site/",
+        path
+      );
+    }
+    const { id: importId } = await this.resolve(path, importee);
+    const contents = await fs.promises.readFile(importId, "utf-8");
+    const assetId = this.emitAsset(attr.value.substr(1), contents);
+    // attr.value = `${placeholderPrefix}${assetId}${placeholderSuffix}`;
+    attr.value = assetId;
+    return attr;
+  }));
+}
+
 export default function htmlPlugin(opts) {
   opts = Object.assign({}, defaultOpts, opts);
   const filter = createFilter(opts.include, opts.exclude);
-
+  
+  const contentMap = new Map();
   return {
     name: "html-plugin",
+    
     async load(id) {
       if (!filter(id)) {
         return;
       }
-      this.warn(id);
       const contents = await fs.promises.readFile(id, { encoding: "utf-8" });
-      const regexp = /<script src="([^"]+)"/g;
-      let match;
-      while ((match = regexp.exec(contents))) {
-        let path = match[1].trim();
-        if (path.startsWith("/")) {
-          path = join(
-            "/Users/surma/src/github.com/surma/surma.github.io/_site/",
-            path
-          );
-        }
-        const { id: importId } = await this.resolve(path, id);
-        const chunkId = this.emitChunk(importId);
-        this.warn(chunkId);
-      }
+      const doc = parseHTML(contents);
+
+      const [scriptChunks, styleChunks] = await Promise.all([
+        findScriptTags.call(this, doc, id),
+        findStylesheets.call(this, doc, id)
+      ]);
+      
+      contentMap.set(id, [doc, scriptChunks, styleChunks]);
+      // Generate some dummy imports in case rollup needs those
       return {
-        code: contents,
-        ast: {
-          type: "Program",
-          sourceType: "module",
-          body: [
-            {
-              type: "ExportDefaultDeclaration",
-              declaration: {
-                type: "FunctionDeclaration",
-                id: null,
-                expression: false,
-                generator: false,
-                params: [],
-                body: {
-                  type: "BlockStatement",
-                  body: []
-                }
-              }
-            }
-          ],
-        }
+        code: ""/*[
+          ...scriptChunks.map((attr, i) => `import(import.meta.ROLLUP_CHUNK_URL_${attr.value});`),
+          ...styleChunks.map((attr, i) => `// import.meta.ROLLUP_ASSET_URL_${attr.value}`),
+        ].join("\n")*/
       };
+    },
+    generateBundle(outputOptions, bundles) {
+      for(const bundleId of Object.keys(bundles)) {
+        const bundle = bundles[bundleId];
+        if(bundle.isAsset) {
+          continue;
+        }
+        if(!contentMap.has(bundle.facadeModuleId)) {
+          continue;
+        }
+        const [doc, scriptChunks, styleChunks] = contentMap.get(bundle.facadeModuleId);
+        for(const scriptChunk of scriptChunks) {
+          scriptChunk.value = this.getChunkFileName(scriptChunk.value);
+        } 
+        for(const styleChunk of styleChunks) {
+          styleChunk.value = this.getAssetFileName(styleChunk.value);
+        } 
+        bundle.code = serializeHTML(doc);
+      }
     }
   };
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,18 @@
 import html from "./rollup-plugins/html.js";
+import css from "./rollup-plugins/css.js";
 
 export default {
   input: ["_site/index.html"],
   output: {
     dir: "_dist",
     format: "esm",
-    entryFileNames: "[name].html"
+    entryFileNames: "[name].html",
+    // We’d need this, tbqh
+    // entryFileNames: "[name].[ext]",
+    // chunkFileNames: "[name]-[hash].[ext]"
   },
   plugins: [
-    html()
+    html(),
+    css()
   ]
 }
