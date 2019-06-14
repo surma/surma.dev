9e6f7ab81d6fe7f8233c218cf9d0072709ec2651
Surma <surma@surma.link>
2019-06-14 16:02:24 +0100

    Fix staging URL injection

--- _data/meta.js
+++ _data/meta.js
@@ -1,7 +1,8 @@
-module.exports = {
-  domain: require("./staging")()
-    ? "https://staging.dassur.ma"
-    : "https://dassur.ma",
-  github: "https://github.com/surma/surma.github.io/",
-  datefmt: { year: "numeric", month: "long", day: "numeric" }
+module.exports = async function() {
+  const isStaging = await require("./staging")();
+  return {
+    domain: isStaging ? "https://staging.dassur.ma" : "https://dassur.ma",
+    github: "https://github.com/surma/surma.github.io/",
+    datefmt: { year: "numeric", month: "long", day: "numeric" }
+  };
 };
