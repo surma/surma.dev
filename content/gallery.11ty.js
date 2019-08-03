440c0ef08d57ac351d1e0526a097697819f1a377
Surma <surma@surma.link>
2019-08-03 19:32:42 +0100

    Small attempt at gallery

--- content/gallery.11ty.js
+++ content/gallery.11ty.js
@@ -1,8 +1,29 @@
 const fsp = require("fs").promises;
 
-module.exports = class Gallers {
-    async render() {
-        const gallery = await fsp.readdir("./static/gallery");
-        return `>> ${gallery.join("|")}`
+async function renderString(template, ...substitution) {
+  substitution = substitution.map(async substitution => {
+    if (Array.isArray(substitution)) {
+      substitution = (await Promise.all(substitution)).join("");
     }
+    return substitution;
+  });
+  return String.raw(template, await Promise.all(substitution));
 }
+
+module.exports = class Gallers {
+  async render() {
+    const gallery = await fsp.readdir("./assets/gallery");
+    return renderString`
+            <link rel="stylesheet" href="emitChunk(/gallery.css)"/>
+            <div class="grid">
+                ${gallery.map(
+                  item => `
+                        <img 
+                            src="/gallery/${item}"
+                        >
+                `
+                )}
+            </div>
+        `;
+  }
+};
