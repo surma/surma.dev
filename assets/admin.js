93cda39bce724514795b0542a7451da6a7bc2cfb
Surma <surma@surma.link>
2019-08-18 15:48:09 +0100

    Creating a file from client side

--- assets/admin.js
+++ assets/admin.js
@@ -1,3 +1,5 @@
+import { decode } from "./jwt.js";
+
 let token;
 
 const TOKEN_KEY = "token";
@@ -8,15 +10,35 @@ if (params.has(TOKEN_KEY)) {
 
 document.querySelector("form").onsubmit = async evt => {
   evt.preventDefault();
-  const formData = new FormData(evt.target);
-  const r = await fetch("/.netlify/functions/new_photo", {
-    method: "POST",
-    body: formData
-  });
+  const { access_token, token_type } = decode(token);
+  const r = await fetch(
+    "https://api.github.com/repos/surma/surma.github.io/contents/test123.txt",
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${token_type} ${access_token}`
+      },
+      body: JSON.stringify({
+        message: "New file from SurmBlog Admin interface",
+        content: btoa("ohai")
+      })
+    }
+  );
   if (!r.ok) {
-    alert("Something went wrong");
+    alert("Gone wrong");
   } else {
-    alert("OK!");
-    // location.reload();
+    alert("ok");
   }
+  // const formData = new FormData(evt.target);
+  // const r = await fetch("/.netlify/functions/new_photo", {
+  //   method: "POST",
+  //   body: formData
+  // });
+  // if (!r.ok) {
+  //   alert("Something went wrong");
+  // } else {
+  //   alert("OK!");
+  //   // location.reload();
+  // }
 };
