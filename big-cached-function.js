f6a723c799773851b5bb9e6f8f3fd424582bcacb
Surma <surma@surma.link>
2019-08-21 23:58:53 +0100

    Put cache in node_modules for Netlify’s build cache

--- big-cached-function.js
+++ big-cached-function.js
@@ -3,7 +3,7 @@ const Cache = require("async-disk-cache");
 module.exports = class BigCachedFunction {
   constructor(name) {
     this.inProgress = new Map();
-    this.cache = new Cache(name, { supportBuffer: true, location: ".cache" });
+    this.cache = new Cache(name, { supportBuffer: true, location: "node_modules/.cache" });
   }
 
   async get(ikey, fn) {
