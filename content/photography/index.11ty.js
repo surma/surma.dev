cbe0c2cf6e5c1078ba285da266444fdda9517937
Surma <surma@surma.link>
2019-09-09 18:23:02 +0100

    Add pagination to gallery

--- content/photography/index.11ty.js
+++ content/photography/index.11ty.js
@@ -6,14 +6,36 @@ const size = 300;
 module.exports = class Gallery {
   data() {
     return {
+      pagination: {
+        data: "collections.photo",
+        size: 9
+      },
+      tags: [],
       layout: "photography"
     };
   }
 
-  async render({ collections }) {
+  async render({ pagination }) {
+    const pageLinksToShow = new Set();
+    pageLinksToShow.add(1);
+    pageLinksToShow.add(pagination.hrefs.length);
+    pageLinksToShow.add(pagination.pageNumber);
+    pageLinksToShow.add(pagination.pageNumber + 1);
+    pageLinksToShow.add(pagination.pageNumber + 2);
+    const pageLinks = [...pageLinksToShow]
+      .sort((a, b) => a - b)
+      .filter(n => n >= 1)
+      .filter(n => n <= pagination.hrefs.length)
+      .reduce((list, p) => {
+        if (p - list[list.length - 1] > 1) {
+          list.push("...");
+        }
+        list.push(p);
+        return list;
+      }, []);
     return html`
       <div class="grid" style="--gallery-size: ${size}px;">
-        ${collections.photo
+        ${pagination.items
           .filter(entry => entry.data.live)
           .sort((a, b) => new Date(b.data.date) - new Date(a.data.date))
           .map(async item => {
@@ -32,6 +54,23 @@ module.exports = class Gallery {
             `;
           })}
       </div>
+      <nav>
+        <a href="${pagination.previousPageHref}">&lt;</a>
+        ${pageLinks.map(l =>
+          l === "..."
+            ? html`
+                <span class="ellipsis">...</span>
+              `
+            : html`
+                <a
+                  href="${pagination.hrefs[l - 1]}"
+                  class="${l - 1 === pagination.pageNumber ? "selected" : ""}"
+                  >${l}</a
+                >
+              `
+        )}
+        <a href="${pagination.nextPageHref}">&gt;</a>
+      </nav>
     `;
   }
 };
