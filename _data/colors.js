e6c96453847bfcf9dd7f010d2cec512329bd4fc2
Surma <surma@surma.link>
2019-10-05 15:25:47 +0100

    Add dark palette

--- _data/colors.js
+++ _data/colors.js
@@ -7,10 +7,19 @@ module.exports = {
     pink: "rgb(191, 85, 105)",
     red: "rgb(218, 59, 59)",
     orange: "rgb(207, 121, 100)",
-    yellow: "rgb(247, 240, 194)"
-  },
-  extras: {
+    yellow: "rgb(247, 240, 194)",
     white: "rgb(251, 248, 228)"
+  },
+  darkpalette: {
+    white: "rgb(47, 43, 69)",
+    yellow: "rgb(25, 23, 37)",
+    orange: "rgb(32, 45, 98)",
+    red: "rgb(38, 57, 125)",
+    pink: "rgb(62, 63, 163)",
+    purple: "rgb(128, 65, 200)",
+    lightblue: "rgb(164, 73, 202)",
+    blue: "rgb(184, 111, 170)",
+    darkblue: "rgb(206, 166, 186)"
   },
   convertToHex(s) {
     return (
