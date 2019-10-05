d964cecaacc31b125112535583ac20207450641f
Surma <surma@surma.link>
2019-10-05 16:40:20 +0100

    Persist dark mode preference

--- assets/darkmode.js
+++ assets/darkmode.js
@@ -1,13 +1,22 @@
+import { get, set } from "idb-keyval";
+
+const KEY = "darkmode";
 async function init() {
+  let darkMode = await get(KEY);
+  if (typeof darkMode !== "boolean") {
+    darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  }
+
   const btn = document.createElement("button");
   btn.textContent = "Toggle dark mode";
   btn.classList.add("darkmode-toggle");
   document.querySelector("header").appendChild(btn);
-  const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
   document.documentElement.classList.toggle("forcedark", darkMode);
-  btn.onclick = () => {
+  btn.onclick = async () => {
     document.documentElement.classList.toggle("forcedark");
     document.documentElement.classList.toggle("forcelight");
+    await set(KEY, document.documentElement.classList.contains("forcedark"));
   };
 }
 init();
