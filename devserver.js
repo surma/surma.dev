86de97e75771158c18c74cbf51d69700c2ee8a35
Surma <surma@surma.link>
2019-10-05 14:46:47 +0100

    Fall back to other ports in devserver

--- devserver.js
+++ devserver.js
@@ -2,17 +2,19 @@ require("dotenv").config();
 
 const { join } = require("path");
 const express = require("express");
+const getPort = require("get-port");
+
 const {
   serveFunctions
 } = require("netlify-dev-plugin/src/utils/serve-functions");
 const proxy = require("express-http-proxy");
 
 async function run() {
-  const { port } = await serveFunctions({
+  const { port: functionsPort } = await serveFunctions({
     functionsDir: join(__dirname, "functions")
   });
   const app = express();
-  app.use("/.netlify/functions/", proxy(`localhost:${port}`));
+  app.use("/.netlify/functions/", proxy(`localhost:${functionsPort}`));
   app.use((req, res, next) => {
     let cache = "no-cache";
     if (/-[0-9a-f]+\.[^.]+$/.test(req.url)) {
@@ -22,7 +24,8 @@ async function run() {
     next();
   });
   app.use(express.static("_site"));
-  const server = app.listen(8080);
+  const port = await getPort({ port: getPort.makeRange(8080, 8888) });
+  const server = app.listen(port);
   console.log(`Server running on http://localhost:${server.address().port}`);
 }
 run();
