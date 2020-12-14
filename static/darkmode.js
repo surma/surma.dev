cbbc68bc10daf770ef2f454f98aa5c7f129c120d
Surma <surma@surma.dev>
2020-12-14 19:16:28 +0000

    Better darkmode toggle without FOUC

--- static/darkmode.js
+++ static/darkmode.js
@@ -1,23 +1,13 @@
-import { get, set } from "idb-keyval";
-
-const KEY = "darkmode";
-async function init() {
-  let darkMode = await get(KEY);
-  if (typeof darkMode !== "boolean") {
-    darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  }
+export async function init(KEY, set) {
+  let darkMode = document.documentElement.classList.contains("forcedark");
 
   const btn = document.createElement("button");
   btn.textContent = "Toggle dark mode";
   btn.classList.add("darkmode-toggle");
   document.querySelector("header").appendChild(btn);
 
-  document.documentElement.classList.toggle("forcedark", darkMode);
-  document.documentElement.classList.toggle("forcelight", !darkMode);
   btn.onclick = async () => {
-    document.documentElement.classList.toggle("forcedark");
-    document.documentElement.classList.toggle("forcelight");
-    await set(KEY, document.documentElement.classList.contains("forcedark"));
+    darkMode = !darkMode
+    set(darkMode);
   };
 }
-init();
