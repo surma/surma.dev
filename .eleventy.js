1de07a37b8f0c08fd7245c5a55e0fb4a2cdbd336
Surma <surma@surma.dev>
2020-12-20 14:03:30 +0000

    Switch to KaTeX

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,20 @@
 const syntaxhighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 require("prismjs/components");
 
+let markdownIt = require("markdown-it");
+let markdownItKatex = require("markdown-it-katex");
+let options = {
+  html: true,
+};
+
+let markdownLib = markdownIt(options).use(markdownItKatex);
 module.exports = function(config) {
   // Copy /static to /
   config.addPassthroughCopy({ 
     static: "/"
   });
   config.addPlugin(syntaxhighlight);
-
+  config.setLibrary("md", markdownLib);
   return {
     dir: {
       input: "content",
