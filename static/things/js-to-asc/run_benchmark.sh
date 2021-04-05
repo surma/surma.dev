537a6c5f694c712c09ca79bd317887ae2d4ece4f
Surma <surma@surma.dev>
2021-04-05 14:35:49 +0100

    Add rust to benchmark script

--- static/things/js-to-asc/run_benchmark.sh
+++ static/things/js-to-asc/run_benchmark.sh
@@ -4,7 +4,7 @@ OUTPUT=${OUTPUT:-"results.csv"}
 echo "Writing to $OUTPUT"
 
 PROGRAMS=${PROGRAMS:-"blur bubblesort binaryheap"}
-VERSIONS=${VERSIONS:-"naive optimized"}
+VERSIONS=${VERSIONS:-"naive idiomatic optimized"}
 RUNTIMES=${RUNTIMES:-"stub minimal incremental"}
 OPTIMIZERS=${OPTIMIZERS:-"O3 O3s"}
 
@@ -12,7 +12,8 @@ echo "Program,Language,Engine,Variant,Optimizer,Runtime" > $OUTPUT
 
 for program in $PROGRAMS ; do
   if [ -f "${program}.js" ]; then
-    if [ -z "${DISABLE_JS}" ]; then
+    # JavaScript
+    if [ -z "${SKIP_JS}" ]; then
       echo -n "${program},JavaScript,Ignition,optimized,,," | tee -a $OUTPUT
       v8 --no-opt --module --harmony-top-level-await ./${program}_js_bench.js >> $OUTPUT
       echo "" 
@@ -21,16 +22,17 @@ for program in $PROGRAMS ; do
       echo "" 
     fi
   fi
+  # AssemblyScript
   for version in $VERSIONS; do
     for runtime in $RUNTIMES; do
       for optimizer in $OPTIMIZERS; do
         SRCFILE="${program}_${version}.ts"
         FILE="./${program}_${version}_${optimizer}_${runtime}.wasm"
-        if [ -f $SRCFILE ]; then
+        if [ -f $SRCFILE ] && [ -z "${SKIP_ASC}" ]; then
           echo "Creating ${FILE}"
           npx asc -b ${program}_${version}_${optimizer}_${runtime}.wasm --runtime ${runtime} -${optimizer} --enable bulk-memory ${SRCFILE}
         fi
-        if [ -z "${COMPILE_ONLY}" ]; then
+        if [ -z "${COMPILE_ONLY}" ] && [ -z "${SKIP_ASC}" ]; then
           if [ -f $FILE ]; then
             echo -n "${program},AssemblyScript,Liftoff,${version},${optimizer},${runtime}," | tee -a $OUTPUT
             v8 --liftoff-only --module --harmony-top-level-await ./${program}_asc_bench.js -- $FILE >> $OUTPUT
@@ -42,5 +44,20 @@ for program in $PROGRAMS ; do
         fi
       done
     done
+  done
+  # Rust
+  for version in $VERSIONS; do
+    SRCFILE="${program}_${version}.rs"
+    FILE="./${program}_${version}_rs.wasm"
+    if [ -f $SRCFILE ] && [ -z "${SKIP_RUST}" ]; then
+      cargo +nightly build --target wasm32-unknown-unknown --release --features ${version} 
+      wasm-opt -O3 target/wasm32-unknown-unknown/release/binaryheap.wasm -o ${FILE}
+      echo -n "${program},Rust,Liftoff,${version},,," | tee -a $OUTPUT
+      v8 --liftoff-only --module --harmony-top-level-await ./${program}_asc_bench.js -- $FILE >> $OUTPUT
+      echo ""
+      echo -n "${program},Rust,Turbofan,${version},,," | tee -a $OUTPUT
+      v8 --no-liftoff --no-wasm-tier-up --module --harmony-top-level-await ./${program}_asc_bench.js -- $FILE >> $OUTPUT
+      echo ""
+    fi
   done
 done
