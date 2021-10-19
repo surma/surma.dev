bd2d6bb61c38f5ab2eb4e17162318847377a5a13
Surma <surma@surma.dev>
2021-10-19 17:42:32 +0100

    Working on ditherpunk2

--- static/lab/ditherpunk2/color.js
+++ static/lab/ditherpunk2/color.js
@@ -1,111 +1,73 @@
-import {
-  blobToImageData,
-  imageDataToPNG,
-  imageToImageData,
-  GrayImageF32N0F8
-} from "../ditherpunk/image-utils.js";
-import { message } from "../ditherpunk/worker-utils.js";
+import Color from "colorjs.io/color.js";
 
-const {
-  fileinput,
-  log,
-  results,
-  examplebtn,
-  exampleimg,
-  bluenoiseimg
-} = document.all;
-let worker;
-  worker = new Worker(new URL("./color-worker.js", import.meta.url), { type: "module" });
-worker.addEventListener("message", async ev => {
-  const { id, type, title, imageData } = ev.data;
-  const [scheme, numColor, algo] = id.split(":");
-  let table = document.querySelector(`#${scheme}`);
-  if (!table) {
-    table = document.createElement("table");
-    table.id = scheme;
-    results.append(table);
+function clamp(min, v, max) {
+  if(v < min) {
+    return min;
   }
-  let container = table.querySelector(`#${algo}`);
-  if (!container) {
-    container = document.createElement("tr");
-    container.id = algo;
-    table.append(container);
+  if(v > max) {
+    return max;
   }
-  let cell = container.getElementsByClassName(numColor)[0];
-  if (!cell) {
-    cell = document.createElement("td");
-    cell.classList.add(numColor);
-    container.append(cell);
-  }
-  while (cell.firstChild) {
-    cell.firstChild.remove();
-  }
-  switch (type) {
-    case "started":
-      cell.innerHTML = "Processing...";
-      break;
-    case "result":
-      const imgUrl = URL.createObjectURL(await imageDataToPNG(imageData));
-      cell.innerHTML = `<img src="${imgUrl}">`;
-      break;
-  }
-  cell.innerHTML += `<br>${title}`;
-});
-worker.addEventListener("error", () =>
-  console.error("Something went wrong in the worker")
-);
-
-function dither(image) {
-  worker.postMessage({
-    id: "image",
-    image
-  });
+  return v;
 }
 
-examplebtn.addEventListener("click", async () => {
-  try {
-    const imgData = await imageToImageData(exampleimg);
-    dither(imgData);
-  } catch (e) {
-    log.innerHTML += `${e.message}\n`;
-  }
-});
-
-fileinput.addEventListener("change", async () => {
-  const file = fileinput.files[0];
-  try {
-    const imgData = await blobToImageData(file);
-    dither(imgData);
-  } catch (e) {
-    log.innerHTML += `${e.message}\n`;
-  }
-});
-
-bluenoiseimg.decode().then(() => {
-  const mask = GrayImageF32N0F8.fromImageData(imageToImageData(bluenoiseimg));
-  worker.postMessage({ mask, id: "bluenoise" });
-});
-
-const numBayerLevels = 4;
-let bayerWorker;
-if (typeof process !== "undefined" && process.env.TARGET_DOMAIN) {
-  bayerWorker = new Worker("../ditherpunk/bayer-worker.js", { name: "bayer" });
-} else {
-  bayerWorker = new Worker("../ditherpunk/bayer-worker.js", {
-    name: "bayer",
-    type: "module"
-  });
+function srgbToCss(color) {
+  return `rgb(${color.map(v => clamp(0, v*255, 255)).join(",")})`;
 }
-bayerWorker.addEventListener("error", () =>
-  console.error("Something went wrong in the Bayer worker")
-);
-const bayerLevels = Array.from({ length: numBayerLevels }, (_, id) => {
-  bayerWorker.postMessage({
-    level: id,
-    id
+
+/**
+ * @param {CanvasRenderingContext2D} ctx
+ */
+function drawText(ctx, text, {textColor = "white", backgroundColor = "black", x = 0, y = 0, fontFamily = "sans-serif", fontSize = 16} = {}) {
+  ctx.save();
+  ctx.textBaseline = "top";
+  ctx.font = `${fontSize}px ${fontFamily}`;
+  const box = ctx.measureText(text);
+  ctx.fillStyle = backgroundColor;
+  ctx.strokeStyle = "none";
+  ctx.fillRect(x, y, box.width, box.actualBoundingBoxDescent);
+  ctx.fillStyle = textColor;
+  ctx.fillText(text, x, y);
+  ctx.restore();
+}
+
+function colorMix(srgbA, srgbB, {unit = 256, interp = "pixelated"} = {}) {
+  const cvs = document.createElement("canvas");
+  cvs.style.imageRendering = interp;
+  cvs.width = 3*unit;
+  cvs.height = unit;
+  // cvs.style.width = `${cvs.width / devicePixelRatio}px`;
+  // cvs.style.height = `${cvs.height / devicePixelRatio}px`;
+  const ctx = cvs.getContext("2d");
+  ctx.fillStyle = srgbToCss(srgbA);
+  ctx.fillRect(0, 0, unit, unit);
+  ctx.fillStyle = srgbToCss(srgbB);
+  for(let y = 0; y < unit; y++) {
+    for(let x = y%2; x < unit; x+=2) {
+      ctx.fillRect(x, y, 1, 1);
+    }
+  }
+  drawText(ctx, "Alternating pixels", {x: 0, y: 0});
+
+  ["srgb", "xyz"].forEach((space, i, arr) => {
+    let middle = Color.mix(srgbA, srgbB, 0.5, {space}).to("srgb");
+    ctx.fillStyle = middle.toString({format: "hex"}); 
+    const x = unit;
+    const y = i * unit / arr.length;
+    ctx.fillRect(x, y, unit, unit/arr.length);
+    drawText(ctx, `50/50 mix in ${space}`, {x, y});
   });
-  return message(bayerWorker, id).then(m => m.result);
-});
-Promise.all(bayerLevels).then(bayerLevels =>
-  worker.postMessage({ bayerLevels, id: "bayerlevels" })
-);
+
+
+  let i = 0;
+  requestAnimationFrame(function f() {
+    i = (i+1)%2;
+    ctx.fillStyle = srgbToCss([srgbA, srgbB][i]);
+    ctx.fillRect(unit*2, 0, unit, unit);
+    drawText(ctx, `Flickering`, {x: unit * 2, y: 0});
+    requestAnimationFrame(f);
+  });
+  return cvs;
+}
+
+document.body.append(colorMix([1, 1, 1], [0, 0, 0]));
+document.body.append(colorMix([1, 1, 1], [0, 0, 0], {interp: "auto"}));
