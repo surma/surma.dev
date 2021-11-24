2cff8b81b9af3a089b337b30c167420e84dc2c5a
Surma <surma@surma.dev>
2021-11-24 09:15:22 +0000

    Prettier

--- static/lab/films/script.js
+++ static/lab/films/script.js
@@ -1,13 +1,11 @@
 import "two-up-element/dist/two-up.js";
 import "pinch-zoom-element/dist/pinch-zoom.js";
 
-
-const {left, right, leftsettings, settings, twoup} = document.all;
+const { left, right, leftsettings, settings, twoup } = document.all;
 
 const rightsettings = leftsettings.cloneNode(true);
 rightsettings.querySelector("legend").textContent = "Right";
 settings.append(rightsettings);
-
 
 left.addEventListener("change", () => right.setTransform(left));
 right.addEventListener("change", () => left.setTransform(right));
@@ -22,8 +20,10 @@ settings.addEventListener("change", () => {
   right.querySelector("img").src = assembleFileName(rightsettings);
 });
 
-document.body.addEventListener("keydown", ev => {
-  if(ev.code === "Digit1") twoup.style = `--split-point: 0px`;
-  if(ev.code === "Digit2") twoup.style = `--split-point: ${twoup.clientWidth/2}px`;
-  if(ev.code === "Digit3") twoup.style = `--split-point: ${twoup.clientWidth}px`;
+document.body.addEventListener("keydown", (ev) => {
+  if (ev.code === "Digit1") twoup.style = `--split-point: 0px`;
+  if (ev.code === "Digit2")
+    twoup.style = `--split-point: ${twoup.clientWidth / 2}px`;
+  if (ev.code === "Digit3")
+    twoup.style = `--split-point: ${twoup.clientWidth}px`;
 });
