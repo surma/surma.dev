ae2187c5883b0b9545351c1951f89ec4f0804d0f
Surma <surma@surma.dev>
2021-12-10 11:13:29 +0000

    Still not quite there

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-const { resolve } = require('path')
-const { defineConfig } = require('vite')
-const glob = require('glob')
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+const glob = require("glob");
 
 const root = resolve(__dirname, ".tmp/");
-const files = glob.sync(resolve(__dirname, "./.tmp/**/*.html"))
+const entryPoints = glob.sync(resolve(__dirname, "./.tmp/**/*.html"));
 
 module.exports = defineConfig({
   build: {
+    assetsInlineLimit: 0,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, '.tmp/index.html'),
-				...Object.fromEntries(
-					files.map(path => ([path.slice(root.length + 1), path]))
-				)
-      }
-    }
-  }
-})
+        main: resolve(__dirname, ".tmp/index.html"),
+        ...Object.fromEntries(
+          entryPoints.map((path) => [path.slice(root.length + 1), path])
+        ),
+      },
+    },
+  },
+});
