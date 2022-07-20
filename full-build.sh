0f9534a341d07d49999b8cb49b713d0dcb590867
Surma <surma@surma.dev>
2022-07-21 00:23:49 +0100

    Fix other shit

---
+++ full-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BRANCH=${BRANCH:-$(git branch --show-current)}
+TARGET_DOMAIN="$BRANCH--surmblog.netlify.app"
+if [ "$BRANCH" = "master" ]; then
+  TARGET_DOMAIN="surma.dev"
+fi
+
+export TARGET_DOMAIN
+export BRANCH
+
+PUBLIC_URL=${PUBLIC_URL:-"https://${TARGET_DOMAIN}"}
+npx eleventy
+npx vite build
