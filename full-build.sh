77d53410a69a8213f5d55a9a7ff7d0d66b4101a3
Surma <surma@surma.dev>
2025-06-17 17:11:34 +0100

    Undo some of the crazy

--- full-build.sh
+++ full-build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-BRANCH=$(echo ${BRANCH:-$(git branch --show-current)} | sed -r 's!/!-!g')
+BRANCH=${BRANCH:-$(git branch --show-current)}
 TARGET_DOMAIN="$BRANCH--surmblog.netlify.app"
 if [ "$BRANCH" = "master" ]; then
   TARGET_DOMAIN="surma.dev"
